e6db8bd4338b1c1ef3d01ddd6b8e16113f32e489
gghosth <42755106+gghosth@users.noreply.github.com>
2019-03-23 23:38:12 -0300

    Primeiros passos

---
+++ Mods.py
@@ -0,0 +1,20 @@
+
+
+
+print("Bem vindo ao programa para fazer uma lista de mods")
+
+print("Para adicionar algo, digite 1, para listar, digite 2, para sair digite outra coisa.\n")
+
+choice = input("Escolha: ")
+
+if(choice == '1'):
+    newmod= input ("Digite o mod para adicionar: ")
+
+    print(newmod)
+
+elif(choice == '2'):
+    print("Os mods atuais são: ")
+
+
+else:
+    print("Obrigado por usar o programa!\n")
